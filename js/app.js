c99a0f3e7e1e25a22e14d42892e9b495cb3d9b9a
zaidalasfar97 <z.alasfar@hotmail.com>
2021-01-04 15:59:47 +0200

    get more information

---
+++ js/app.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var username=prompt('Wecome here , What is your name ');
+alert('Welcome her ' +username )
+
+
+
+var Uage = prompt('Are you over 18 years old ');
+
+if(Uage.toLowerCase() === 'yes' || Uage.toUpperCase()=== 'yes' || Uage.toLowerCase()==='y' || Uage.toUpperCase()=== 'y'){
+  alert('You are over 18 years old ');
+} else {
+  alert('You are under 18 years old ' );
+}
+// console.log(Uage);
+
+var Uscholl = prompt ('Are you studing in shcool?');
+
+if (Uscholl.toLowerCase() === 'yes' || Uscholl.toUpperCase()=== 'yes' || Uscholl.toLowerCase()==='y' || Uscholl.toUpperCase()=== 'y'){
+    alert('You are in school');
+  } else {
+    alert('You are not in  school' );
+
+  }
+
+ // console.log(Uscholl);
+
+
+  var Uuni = prompt ('Are you in University ? ');
+
+  if (Uscholl.toLowerCase() === 'yes' || Uscholl.toUpperCase()=== 'yes' || Uscholl.toLowerCase()=== 'y' || Uscholl.toUpperCase()=== 'y'){
+    alert('You are in University');
+  } else {
+    alert('You are not in  Unviersity' );
+
+  }
+
+  //console.log(Uuni);
+
+
+  var animals = prompt('Do you love dog?');
+
+if (animals.toLowerCase() === 'yes' || animals.toUpperCase()=== 'yes' || animals.toLowerCase()==='y' || animals.toUpperCase()=== 'y'){
+    alert('Wow , you love dogs ');
+  } else {
+    alert('Ohh , you arent love dogs ' );
+
+  }
+
+  // console.log(animals);
+
+
+  var cats = prompt('Do you love cats ?');
+
+  if (cats.toLowerCase() === 'yes' || cats.toUpperCase()=== 'yes' || cats.toLowerCase()==='y' || cats.toUpperCase()=== 'y'){
+    alert('Wow , you love cats ');
+  } else {
+    alert('Ohh , you arent love cats' );
+
+  }
+
+  // console.log(cats);
+
+
